a1f973dbe7c2a4898b2543cde4da4749eb2c5a7d
marciohubees <marcio@netpark.com.br>
2024-10-30 21:32:30 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PushNotifications } from '@capacitor/push-notifications';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
@@ -9,4 +10,22 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 })
 export class AppComponent {
   constructor() {}
+
+  ngOnInit() {
+    this.initializePushNotifications();
+  }
+
+  private async initializePushNotifications() {
+    const permissionStatus = await PushNotifications.checkPermissions();
+
+    if (permissionStatus.receive === 'granted') {
+    } else {
+      const result = await PushNotifications.requestPermissions();
+      if (result.receive === 'granted') {
+        console.log('Push notification permission granted');
+      } else {
+        console.log('Push notification permission not granted');
+      }
+    }
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { HubeesNotification } from 'plugins/hubees-notification/src';
 
 @Component({
   selector: 'app-home',
@@ -10,4 +11,48 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
 })
 export class HomePage {
   constructor() {}
+
+  startService() {
+    const timeStart = "09:00";
+    const timeEnd = "12:00";
+    const remainingTime = "2h05";
+    const details = "Permanência: 00:30";
+    const arrivalTime = "Chegada: 09:00";
+    const progress = 30;
+
+    HubeesNotification.sendNotification({
+      timeStart,
+      timeEnd,
+      remainingTime,
+      arrivalTime,
+      details,
+      progress
+    }).then(result => {
+      console.log('Notification result:', result);
+    }).catch(error => {
+      console.error('Error sending notification:', error);
+    });
+  }
+
+  updateService() {
+    const timeStart = "12:00";
+    const timeEnd = "13:00";
+    const remainingTime = "2h35";
+    const details = "Permanência: 10:55";
+    const arrivalTime = "Chegada: 10:00";
+    const progress = 80;
+
+    HubeesNotification.updateNotification({
+      timeStart,
+      timeEnd,
+      remainingTime,
+      arrivalTime,
+      details,
+      progress
+    }).then(result => {
+      console.log('Notification result:', result);
+    }).catch(error => {
+      console.error('Error sending notification:', error);
+    });
+  }
 }
